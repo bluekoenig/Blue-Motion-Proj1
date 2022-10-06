5279b7004dc446a5e6578bd6b5d6d504e69cf416
Blue <bk647@drexel.edu>
2022-10-06 09:49:37 -0400

    hr

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,7 +3,7 @@ import { gsap } from "gsap";
 gsap.set("button i",{transformOrigin:"center bottom"});
 
 gsap.from(".slogan",{duration:0.5, alpha:0, y:100});
-gsap.from(".fish-logo",{duration:0.5, alpha:0, y:-50, delay:0.25});
+gsap.from(".fish-logo",{duration:0.75, alpha:0, y:-50, delay:0.25});
 gsap.from(".fish-but",{duration:0.5, alpha:0, y:50, delay:0.35});
 gsap.from(".fish-but i",{duration:0.25, alpha:0, rotation:-90, delay:0.45});
 
@@ -13,7 +13,7 @@ button.addEventListener("mouseover",function(){
     gsap.to(".fish-but i",{duration:0.40, rotateY: 360});
 
     gsap.to(".slogan",{duration:0.5, alpha:0, x:200});
-    gsap.to("#second-line",{duration:0.25, alpha:0, y:25});
+    gsap.to(".fish-logo",{duration:0.25, alpha:0, y:25});
 
 });
 
@@ -22,5 +22,5 @@ button.addEventListener("mouseout",function(){
     gsap.to(".fish-but i",{duration:0.25, x:50});
 
     gsap.to(".slogan",{duration:0.25, alpha:1, x:0});
-    gsap.to("#second-line",{duration:0.25, alpha:1, y:0});
+    gsap.to(".fish-logo",{duration:0.25, alpha:1, y:0});
 });
